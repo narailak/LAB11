b39d04d43c856b1723aa1dd7f1e34b908cbb39dd
OATA <narailak_s@cmu.ac.th>
2023-12-26 12:45:34 +0700

    finish

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,18 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    srand(time(0));
+    string grade [] = {"A","B+","B","C+","C","D+","D","F","W"};
+    int i = rand() %9;
+    cout << "Press Enter 3 times to reveal your future.";
+    for(int b =0;b< 3;b++)
+    {
+        cin.get();
+    }
+    cout << "You will get " << grade[i] <<" in this 261102.";
+}
